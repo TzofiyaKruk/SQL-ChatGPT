ddd85c7352b57a4214095804b673aee147cf1a6b
root <tzofiya.kruk@gmail.com>
2024-03-31 00:30:59 +0300

    sql-chatgpt commit 2

---
+++ src/main/java/com/handson/sqlchatgpt/SqlChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.handson.sqlchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlChatgptApplication.class, args);
+	}
+
+}
